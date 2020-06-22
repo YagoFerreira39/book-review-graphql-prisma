940807710d37ec5f33d3f073fbe7773dd79a9e72
Yfer <yago@codeby.com.br>
2020-06-22 18:20:44 -0300

    Prima types and mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,34 @@
 import hashPassword from "../utils/hashPassword";
 import generateToken from "../utils/generateToken";
+import getUserId from "../utils/getUserId";
+import bcrypt from "bcryptjs";
 
 const Mutation = {
+  async login(parent, args, { prisma }, info) {
+    const user = await prisma.query.user({
+      where: {
+        email: args.data.email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User cannot be found.");
+    }
+
+    const validPassword = await bcrypt.compare(
+      args.data.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      throw new Error("Unable to login");
+    }
+
+    return {
+      user,
+      token: generateToken(user.id),
+    };
+  },
   async createUser(parent, args, { prisma }, info) {
     const emailTaken = await prisma.exists.User({ email: args.data.email });
 
@@ -14,7 +41,7 @@ const Mutation = {
       data: {
         name: args.data.name,
         email: args.data.email,
-        password,
+        password: await hashPassword(args.data.password),
       },
     });
 
@@ -23,8 +50,8 @@ const Mutation = {
       token: generateToken(user.id),
     };
   },
-
-  async deleteUser(parent, args, { prisma }, info) {
+  async deleteUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
     const userExists = await prisma.exists.User({ id: args.id });
 
     if (!userExists) {
@@ -34,7 +61,144 @@ const Mutation = {
     return prisma.mutation.deleteUser(
       {
         where: {
+          id: userId,
+        },
+      },
+      info
+    );
+  },
+  async updateUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    if (typeof args.data.password === "string") {
+      args.data.password = hashPassword(args.data.password);
+    }
+
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: userId,
+        },
+        data: data.args,
+      },
+      info
+    );
+  },
+
+  async createAuthor(parent, args, { prisma }, info) {
+    return prisma.mutation.createAuthor(
+      {
+        data: {
+          name: args.data.name,
+          about: args.data.about,
+        },
+      },
+      info
+    );
+  },
+  async deleteAuthor(parent, args, { prisma }, info) {
+    const authorExists = await prisma.exists.Author({ id: args.id });
+
+    if (!authorExists) {
+      throw new Error("Author cannot be found.");
+    }
+
+    return prisma.mutation.deleteAuthor(
+      {
+        where: {
+          id: args.data.id,
+        },
+      },
+      info
+    );
+  },
+  async updateAuthor(parent, args, { prisma }, info) {
+    return prisma.mutation.updateAuthor(
+      {
+        where: {
           id: args.id,
+        },
+        data: args.data,
+      },
+      info
+    );
+  },
+
+  async createBook(parent, args, { prisma }, info) {
+    return prisma.mutation.createBook(
+      {
+        data: {
+          title: args.data.title,
+          sinopse: args.data.sinopse,
+          published: args.data.published,
+          genre: args.data.genre,
+          pages: args.data.pages,
+          author: {
+            connect: {
+              id: args.data.author,
+            },
+          },
+        },
+      },
+      info
+    );
+  },
+  async deleteBook(parent, args, { prisma }, info) {
+    const bookExists = await prisma.exists.Book({ id: args.id });
+
+    if (!bookExists) {
+      throw new Error("Book cannot be found.");
+    }
+
+    return prisma.mutation.deleteBook(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async updateBook(parent, args, { prisma }, info) {
+    return prisma.mutation.updateBook(
+      {
+        where: {
+          id: args.id,
+        },
+        data: args.data,
+      },
+      info
+    );
+  },
+
+  async wantToRead(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    const bookExists = await prisma.exists.Book({ id: args.id });
+    if (!bookExists) {
+      throw new Error("Book cannot be found.");
+    }
+
+    const book = await prisma.query.books({
+      where: {
+        id: args.id,
+      },
+    });
+
+    if (!book) {
+      throw new Error("Book cannot be found.");
+    }
+
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: userId,
+        },
+        data: {
+          wantToRead: {
+            connect: {
+              id: args.id,
+            },
+          },
         },
       },
       info
